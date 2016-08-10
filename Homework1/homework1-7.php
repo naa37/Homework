35ff1f93f8ae9eeadc245626135d756364edd8f7
Alexander Nikonov <naa37@yandex.ru>
2016-08-10 22:43:59 +0300

    add table tag

--- Homework1/homework1-7.php
+++ Homework1/homework1-7.php
@@ -1,12 +1,18 @@
 <?php
 
+echo '<table border="1px" cellspacing="0" bordercolor="darkblue" bgcolor="#fff8dc">';
 for ($x = 1; $x <= 10; $x++)
 {
+    echo '<tr>';
     for ($y = 1; $y <= 10; $y++)
     {
+        echo '<td style="padding: 5px">';
         $mult = $x * $y;
-        if ($x % 2 == 0 and $y % 2 == 0) echo "$x x $y = (", $mult, ')', '<br>';
-        elseif ($x % 2 <> 0 and $y % 2 <> 0) echo "$x x $y = [", $mult, ']', '<br>';
-        else echo "$x x $y = ", $mult, '<br>';
+        if ($x % 2 == 0 and $y % 2 == 0) echo "$y x $x = (", $mult, ')', '<br>';
+        elseif ($x % 2 <> 0 and $y % 2 <> 0) echo "$y x $x = [", $mult, ']', '<br>';
+        else echo "$y x $x = ", $mult, '<br>';
+        echo '</td>';
     }
+    echo '</tr>';
 }
+echo '</table>';
