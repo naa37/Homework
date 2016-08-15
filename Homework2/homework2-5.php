b1628076b307080dff0b863d01bb16c6bf38d1b2
Alexander Nikonov <naa37@yandex.ru>
2016-08-15 08:36:28 +0300

    first commit

---
+++ Homework2/homework2-5.php
@@ -0,0 +1,19 @@
+<?php
+
+function check($str) {
+    $str = mb_strtolower(str_replace(' ', '', $str));
+    $newStr = stringrev($str);
+    if (strcasecmp($str, $newStr) == 0) return true;
+    else return false;
+}
+function printer($bool) {
+    if (is_bool($bool) and $bool) echo 'Строка является палиндромом';
+    elseif (is_bool($bool) and !$bool) echo 'Строка не является палиндромом';
+    else echo 'Передан неверный параметр!';
+}
+function stringrev($str)
+{
+    preg_match_all('/./us', $str, $arr);
+    return implode(array_reverse($arr[0]));
+}
+printer(check('!@ Ах аха   @ !'));
